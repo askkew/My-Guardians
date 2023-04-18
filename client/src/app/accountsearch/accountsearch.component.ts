7aee868ccfb6ae17e024d97dbbafaaef0592a2c9
Lucas Overbey <lucasaoverbey@gmail.com>
2023-04-17 21:25:31 -0500

    moved search function to another component

--- client/src/app/accountsearch/accountsearch.component.ts
+++ client/src/app/accountsearch/accountsearch.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-accountsearch',
   templateUrl: './accountsearch.component.html',
   styleUrls: ['./accountsearch.component.css']
 })
-export class AccountsearchComponent {
+export class AccountsearchComponent implements OnInit {
+  displayName: string;
+  displayNameCode: number;
+  characterList: any[] = [];
+  characterInventory: any[] = [];
 
+  constructor(private http: HttpClient) {
+    this.displayName = '';
+    this.displayNameCode = 0;
+    this.characterList = [];
+    this.characterInventory = [];
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    const data = {
+      displayName: this.displayName,
+      displayNameCode: this.displayNameCode,
+    };
+  
+    this.http.post<any>(`http://localhost:5000/search-and-inventory`, data).subscribe(
+      response => {
+        console.log(response);
+        const characterData = response.characters;
+        const characterList = Object.values(characterData);
+        this.characterList = characterList;
+        const characterInventory = response.inventorys;
+        this.characterInventory = characterInventory;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  getClassName(classHash: number): string {
+    switch (classHash) {
+      case 2271682572:
+        return 'Warlock';
+      case 671679327:
+        return 'Hunter';
+      case 3655393761:
+        return 'Titan';
+      default:
+        return 'Unknown';
+    }
+  }
+  
+  getRaceName(raceHash: number): string {
+    switch (raceHash) {
+      case 3887404748:
+        return 'Human';
+      case 2803282938:
+        return 'Awoken';
+      case 898834093:
+        return 'Exo';
+      default:
+        return 'Unknown';
+    }
+  }
 }
